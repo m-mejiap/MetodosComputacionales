1b36d0b9a899747a8fd0db9dfb5173d8755143a0
Default user <m.mejiap@uniandes.edu.co>
2019-07-02 16:47:03 +0000

    producto_array_FUN.cpp

--- S5C1/producto_array_FUN.cpp
+++ S5C1/producto_array_FUN.cpp
@@ -1,21 +1,40 @@
-// Haga un programa que tenga dos funciones. Cada una debe recibir dos arreglos unidimensionales de 5 elementos cada uno. La primera funcion debe retornar el producto de los dos arreglos elemento a elemento. La segunda debe retornar el producto punto de los dos arreglos. En la funcion main se debe imprimir lo que retornen las dos funciones para [1,2,3,4,5] y [10,20,30,40,50]. llamelo producto_array_FUN.cpp
-
 #include <iostream>
 using namespace std;
 
-int p_elemento(int in1[5],int in1[5]);
-int p_punto(int in2[5],int in1[5]);
+int * p_elemento(int in1[], int in2[]);
+int p_punto(int in1[], int in2[]);
 
 int main(){
-    cout<<"El producto elemento a elemento de los dos arreglos es: "<< "" <<endl;
-    cout<<"El producto putno de los elementos es es: "<< "" <<endl;
+    int a1[5] = {1,2,3,4,5};
+    int a2[5] = {10,20,30,40,50};
+    cout<<"El producto elemento a elemento de los dos arreglos es: ";
+    for(int i=0; i<5; i++){
+        if (i<4){
+            cout<<p_elemento(a1,a2)[i]<<",";
+        }
+        else{
+            cout<<p_elemento(a1,a2)[i]<<".";
+        }
+    }
+    cout<<endl;
+    cout<<"El producto punto de los elementos es es: "<< p_punto(a1,a2) << "." <<endl;
     return 0;
 }
 
-int p_elemento(int in1[5],int in1[5]){
-    return 1;
+int * p_elemento(int in1[],int in2[]){
+    int producto[5];
+    int *point;
+    point = producto;
+    for(int i=0; i<5; i++){
+        producto[i] = in1[i] * in2[i];
+    }
+    return point;
 }
 
-int p_punto(int in2[5],int in1[5]){
-    
+int p_punto(int in1[],int in2[]){
+    int producto = 0;
+    for(int i=0; i<5; i++){
+        producto = producto + (in1[i] * in2[i]);
+    }
+    return producto;
 }
