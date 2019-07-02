65849311fee8c8c9c45e0b8e850f985231d0d810
Default user <m.mejiap@uniandes.edu.co>
2019-07-02 15:52:46 +0000

    producto_array_FUN.cpp

--- S5C1/producto_array_FUN.cpp
+++ S5C1/producto_array_FUN.cpp
@@ -1,1 +1,21 @@
-A
+// Haga un programa que tenga dos funciones. Cada una debe recibir dos arreglos unidimensionales de 5 elementos cada uno. La primera funcion debe retornar el producto de los dos arreglos elemento a elemento. La segunda debe retornar el producto punto de los dos arreglos. En la funcion main se debe imprimir lo que retornen las dos funciones para [1,2,3,4,5] y [10,20,30,40,50]. llamelo producto_array_FUN.cpp
+
+#include <iostream>
+using namespace std;
+
+int p_elemento(int in1[5],int in1[5]);
+int p_punto(int in2[5],int in1[5]);
+
+int main(){
+    cout<<"El producto elemento a elemento de los dos arreglos es: "<< "" <<endl;
+    cout<<"El producto putno de los elementos es es: "<< "" <<endl;
+    return 0;
+}
+
+int p_elemento(int in1[5],int in1[5]){
+    return 1;
+}
+
+int p_punto(int in2[5],int in1[5]){
+    
+}
