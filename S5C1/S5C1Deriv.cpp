ef17b0d6c935b4dd57eef5b7cce9f0e0fd243011
m-mejiap <52307741+m-mejiap@users.noreply.github.com>
2019-07-03 18:21:45 -0500

    Update S5C1Deriv.cpp

--- S5C1/S5C1Deriv.cpp
+++ S5C1/S5C1Deriv.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 
-int derivada(int a, int b, int n);
+int * derivada(int a, int b, int n);
 int a0;
 int b0;
 int n0;
@@ -14,10 +15,40 @@ int main(){
     cout<<"Número de puntos: ";
     cin>>n0;
     cout<<endl;
-    cout<<derivada(a0,b0,n0)<<endl;
+    cout<<"La derivada es: "<<derivada(a0,b0,n0)<<endl;
+    cout<<lin_space(a0,b0,n0)<<endl;
     return 0;
 }
 
-int derivada(int a, int b, int n){
-    return 0;
+int * derivada(int a, int b, int n){
+    double h = (b-a)/n;
+    float der[];
+    int *point = der;
+    return point;
 }
+
+int * lin_space(int a, int b, int n){
+    double s = (b-a)/n;
+    float lins[];
+    float * pointer1 = lins;
+    for(int i=0; i<=(b-a); i++){
+        lins[i] = i * s;
+        cout<<lins[i];
+    }
+    return pointer1;
+}
+
+
+
+//.   def funcion(x):
+//.       return np.cos(x)
+//.   
+//.   n = 1000
+//.   h = (2*np.pi)/n
+//.   arr = np.linspace(0,2*np.pi,num=n)
+//.   
+//.   def central():
+//.       der = []
+//.       for i in arr:
+//.           der.append((funcion(i+h/2)-funcion(i-h/2))/h)
+//.       return der
