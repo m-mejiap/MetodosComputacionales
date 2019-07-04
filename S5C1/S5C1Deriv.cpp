35cb1629510848d420604bdef77813df30ef9485
m-mejiap <52307741+m-mejiap@users.noreply.github.com>
2019-07-03 20:11:31 -0500

    Update S5C1Deriv.cpp

--- S5C1/S5C1Deriv.cpp
+++ S5C1/S5C1Deriv.cpp
@@ -2,7 +2,9 @@
 #include <cmath>
 using namespace std;
 
-int * derivada(int a, int b, int n);
+double * lin_space(int a, int b, int n);
+double * funcion(float x[]);
+double * derivada(int a, int b, int n);
 int a0;
 int b0;
 int n0;
@@ -15,40 +17,46 @@ int main(){
     cout<<"Número de puntos: ";
     cin>>n0;
     cout<<endl;
-    cout<<"La derivada es: "<<derivada(a0,b0,n0)<<endl;
     cout<<lin_space(a0,b0,n0)<<endl;
+    cout<<funcion(lin_space(a0,b0,n0),n0)<<endl;
+    cout<<derivada(a0,b0,n0)<<endl;
     return 0;
 }
 
-int * derivada(int a, int b, int n){
-    double h = (b-a)/n;
-    float der[];
-    int *point = der;
-    return point;
-}
-
-int * lin_space(int a, int b, int n){
+double * lin_space(int a, int b, int n){
     double s = (b-a)/n;
-    float lins[];
-    float * pointer1 = lins;
-    for(int i=0; i<=(b-a); i++){
-        lins[i] = i * s;
-        cout<<lins[i];
+    double lins[n];
+    double * pointer1 = lins;
+    for(int i=0; i<=n; i++){
+        lins[i] = a + (i * s);
     }
     return pointer1;
 }
 
+double * funcion(float x[], int n){
+    double coseno[n];
+    double * pointer2 = coseno;
+    for(int i=0; i<=n; i++){
+        coseno[i] = cos(x[i]);
+        cout<<coseno[i]<<endl;
+    }
+}
+
+double * derivada(int a, int b, int n){
+    double h = (b-a)/n;
+    double der[n];
+    double *point = der;
+    return point;
+}
 
 
-//.   def funcion(x):
-//.       return np.cos(x)
-//.   
-//.   n = 1000
-//.   h = (2*np.pi)/n
-//.   arr = np.linspace(0,2*np.pi,num=n)
-//.   
-//.   def central():
-//.       der = []
-//.       for i in arr:
-//.           der.append((funcion(i+h/2)-funcion(i-h/2))/h)
-//.       return der
+//x = np.array([])
+//x = np.append(x, np.genfromtxt('datosfun.dat',usecols=0))
+//fx = np.array([])
+//fx = np.append(fx, np.genfromtxt('datosfun.dat',usecols=1))
+//
+//fxh = np.array([])
+//fxh = np.append(fxh, fx[1:])
+//h = np.absolute(x[1]-x[0])
+//uhh =  fxh - fx[0:-1]
+//derivada = uhh/h
