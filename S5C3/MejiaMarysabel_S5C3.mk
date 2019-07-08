91c52913dda8ee42586087c7f122683a7f549d9d
m-mejiap <52307741+m-mejiap@users.noreply.github.com>
2019-07-07 21:44:19 -0500

    Update MejiaMarysabel_S5C3.mk

--- S5C3/MejiaMarysabel_S5C3.mk
+++ S5C3/MejiaMarysabel_S5C3.mk
@@ -1,8 +1,8 @@
 #Escriba un Makefile llamado ApellidoNombre_S5C3.mk que genere la grafica, datos y maneje las dependencias, etc...
 
 data.dat : MejiaMarysabel_S5C3_ODEs.cpp
-    g++ MejiaMarysabel_S5C3_ODEs.cpp
-    ./a.out > data.dat
+	g++ MejiaMarysabel_S5C3_ODEs.cpp
+	./a.out > data.dat
     
 MejiaMarysabelResorte.pdf : MejiaMarysabel_S5C3_ODEs.cpp MejiaMarysabel_S5C3_plots.py
-    python MejiaMarysabel_S5C3_plots.py
+	python MejiaMarysabel_S5C3_plots.py
