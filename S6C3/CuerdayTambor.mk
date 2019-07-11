6554e9dbb9e4c620d95cb68f976508705b5458de
Default user <m.mejiap@uniandes.edu.co>
2019-07-11 13:09:23 +0000

    CuerdayTambor.mk

--- S6C3/CuerdayTambor.mk
+++ S6C3/CuerdayTambor.mk
@@ -1,5 +1,11 @@
 #Makefile.
-
-g++ MejiaMarysabel_CuerdayTambor.cpp
-./a.out > data.dat
-python Plots_cuerdayTambor.py
+    
+plots.png : data.dat MejiaMarysabel_S5C3_plots.py
+	python Plots_cuerdayTambor.py
+    
+data.dat : a.out
+	g++ MejiaMarysabel_S5C3_ODEs.cpp
+	./a.out > data.dat
+    
+a.out : MejiaMarysabel_CuerdayTambor.cpp
+	g++ MejiaMarysabel_S5C3_ODEs.cpp
