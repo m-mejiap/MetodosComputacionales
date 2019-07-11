1542a6032b2a09d3aed27529bab1e4b51b3632b6
m-mejiap <52307741+m-mejiap@users.noreply.github.com>
2019-07-11 08:11:15 -0500

    Update CuerdayTambor.mk

--- S6C3/CuerdayTambor.mk
+++ S6C3/CuerdayTambor.mk
@@ -4,7 +4,6 @@ plots.png : data.dat MejiaMarysabel_S5C3_plots.py
 	python Plots_cuerdayTambor.py
     
 data.dat : a.out
-	g++ MejiaMarysabel_S5C3_ODEs.cpp
 	./a.out > data.dat
     
 a.out : MejiaMarysabel_CuerdayTambor.cpp
