8593e48026c627ea8610e9da3d75760540abeefb
Default user <m.mejiap@uniandes.edu.co>
2019-07-16 21:17:27 +0000

    ODEs.cpp

--- Tarea 2/ODEs.cpp
+++ Tarea 2/ODEs.cpp
@@ -1,3 +1,25 @@
 #include <iostream>
 using namespace std;
 
+int Euler();
+int Leap();
+int Kutta();
+
+int main(){
+    cout<<Euler()<<endl;
+    cout<<Leap()<<endl;
+    cout<<Kutta()<<endl;
+    return 0;
+}
+
+int Euler(){
+    return 0;
+}
+
+int Leap(){
+    return 0;
+}
+
+int Kutta(){
+    return 0;
+}
