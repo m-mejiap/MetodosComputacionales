47c5e65cd31cad0652d078028cce9970af713c31
Default user <m.mejiap@uniandes.edu.co>
2019-07-18 04:25:39 +0000

    MejiaMarysabel_S7C2.py

--- S7C2/MejiaMarysabel_S7C2.py
+++ S7C2/MejiaMarysabel_S7C2.py
@@ -19,18 +19,24 @@ def caminata(numpasos,sigma,x_guess):
     x_viejo = x_guess
     x_almacena = [x_guess]
     for i in range(1,numpasos):
-        x_nuevo = np.random.normal(x_viejo,sigma,1)
+        x_nuevo = np.random.normal(x_viejo,sigma,1)[0]
         alpha = mifun(x_nuevo)/mifun(x_viejo)
-        if(alpha>1):
-            x_almacena.extend(x_nuevo)
+        if(alpha>=1):
+            x_almacena.append(x_nuevo)
         if(alpha<1):
-            beta = np.random.random(1)
+            beta = np.random.random(1)[0]
             if(beta<alpha):
-                x_almacena.extend(x_nuevo)
+                x_almacena.append(x_nuevo)
             if(beta>alpha):
-                x_almacena.extend(x_viejo)
+                x_almacena.append(x_viejo)
         x_viejo = x_nuevo
     return x_almacena
+
+plt.figure()
+plt.plot(x0,mifun(x0),color="black")
+plt.hist(caminata(1000,0.1,-1.0), bins=100, density = True)
+plt.title(u"Gráfica: Histograma 4.")
+plt.savefig("histograma_str(0.1)_str(1000).pdf")
 
 plt.figure()
 plt.plot(x0,mifun(x0),color="black")
@@ -41,29 +47,29 @@ plt.savefig("histograma_str(5)_str(100000).pdf")
 plt.figure()
 plt.plot(x0,mifun(x0),color="black")
 plt.hist(caminata(100000,0.2,-1.0), bins=100, density = True)
-plt.title(u"Gráfica: Histograma 1.")
+plt.title(u"Gráfica: Histograma 2.")
 plt.savefig("histograma_str(0.2)_str(100000).pdf")
 
 plt.figure()
 plt.plot(x0,mifun(x0),color="black")
 plt.hist(caminata(100000,0.01,-1.0), bins=100, density = True)
-plt.title(u"Gráfica: Histograma 1.")
+plt.title(u"Gráfica: Histograma 3.")
 plt.savefig("histograma_str(0.01)_str(100000).pdf")
 
 plt.figure()
 plt.plot(x0,mifun(x0),color="black")
 plt.hist(caminata(1000,0.1,-1.0), bins=100, density = True)
-plt.title(u"Gráfica: Histograma 1.")
+plt.title(u"Gráfica: Histograma 4.")
 plt.savefig("histograma_str(0.1)_str(1000).pdf")
 
 plt.figure()
 plt.plot(x0,mifun(x0),color="black")
 plt.hist(caminata(100000,0.1,-1.0), bins=100, density = True)
-plt.title(u"Gráfica: Histograma 1.")
+plt.title(u"Gráfica: Histograma 5.")
 plt.savefig("histograma_str(0.1)_str(100000).pdf")
 
 plt.figure()
 plt.plot(x0,mifun(x0),color="black")
 plt.hist(caminata(500000,0.1,-1.0), bins=100, density = True)
-plt.title(u"Gráfica: Histograma 1.")
+plt.title(u"Gráfica: Histograma 6.")
 plt.savefig("histograma_str(0.1)_str(500000).pdf")
