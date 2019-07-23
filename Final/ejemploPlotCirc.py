c5c729a59116df916079840793fe169db0afb910
Default user <m.mejiap@uniandes.edu.co>
2019-07-23 16:48:25 +0000

    Final/

---
+++ Final/ejemploPlotCirc.py
@@ -0,0 +1,11 @@
+import numpy as np
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+
+#fig, ax = plt.subplots()
+#plt.axis('equal')
+#circle1 = plt.Circle((best_x, best_y), np.max(r_walk), color='r',fill=False)
+#ax.add_artist(circle1)
+#plt.savefig("lalalalalalala.png")
+#plt.close()
